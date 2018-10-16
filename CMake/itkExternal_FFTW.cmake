c2bc3df97ec9624d423bbda5f323e9fe98e9667c
allywarner <avw0731@gmail.com>
2018-10-16 15:34:44 -0600

    BUG: CMake build errors
    Removed quotes from URL and URL_HASH commands that were causing build errors.

--- CMake/itkExternal_FFTW.cmake
+++ CMake/itkExternal_FFTW.cmake
@@ -89,8 +89,8 @@ else()
       itk_download_attempt_check(FFTW)
       ExternalProject_add(fftwf
         PREFIX fftwf
-        URL "${_fftw_url}"
-        URL_HASH SHA512="${_fftw_url_hash}"
+        URL ${_fftw_url}
+        URL_HASH SHA512=${_fftw_url_hash}
         DOWNLOAD_NAME "fftw-${_fftw_target_version}.tar.gz"
         CONFIGURE_COMMAND
           env
@@ -117,8 +117,8 @@ else()
       itk_download_attempt_check(FFTW)
       ExternalProject_add(fftwd
         PREFIX fftwd
-        URL "${_fftw_url}"
-        URL_HASH SHA512="${_fftw_url_hash}"
+        URL ${_fftw_url}
+        URL_HASH SHA512=${_fftw_url_hash}
         DOWNLOAD_NAME "fftw-${_fftw_target_version}.tar.gz"
         CONFIGURE_COMMAND
           env
